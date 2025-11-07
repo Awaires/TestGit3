1d06c53a5dc2d6b179a95a57d6ef0a5ed7534557
Evana FERRETTI <ev.ferretti@game-sup.fr>
2025-11-07 16:01:57 +0100

    Added Debug.Log in TestScript

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -5,7 +5,7 @@ public class TestScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("This is a test for Github");
     }
 
     // Update is called once per frame
